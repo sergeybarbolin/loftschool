564ce35c5f2c4aa504f0b327dc43735b39ade557
sergeybarbolin <sergeybarbolin25@gmail.com>
2019-06-24 20:41:31 +0300

    cusom geocoder

--- src/index.js
+++ src/index.js
@@ -18,27 +18,12 @@ const createPlacemark = (coords, geoObject) => {
     return placemark;
 };
 
-const placemarks = [];
-// const geocoder = async coords => {
-//     const coordsStr = coords.reverse().join(',');
-//     const url = `https://geocode-maps.yandex.ru/1.x/?apikey=963c3670-287a-487a-8c60-9f1d43c03028&format=json&results=1&geocode=${coordsStr}`;
-//     const response = await fetch(url);
-//     const data = await response.json();
+const geocoder = coords => {
+    const coordsStr = coords.reverse().join(',');
+    const url = `https://geocode-maps.yandex.ru/1.x/?apikey=963c3670-287a-487a-8c60-9f1d43c03028&format=json&results=1&geocode=${coordsStr}`;
     
-//     return data.response;
-//     // return data.response.GeoObjectCollection.featureMember[0].GeoObject;
-// }
-
-// const geocoder = coords => {
-//     const coordsStr = coords.reverse().join(',');
-//     const url = `https://geocode-maps.yandex.ru/1.x/?apikey=963c3670-287a-487a-8c60-9f1d43c03028&format=json&results=1&geocode=${coordsStr}`;
-    
-//     return fetch(url).then(response => response.json()).then(response => response.response);
-// }
-
-// const oldPlacemarks = JSON.parse(localStorage.getItem('placemarks'));
-
-// console.log(oldPlacemarks);
+    return fetch(url).then(response => response.json()).then(response => response.response);
+}
 
 const init = () => {
     const map = new ymaps.Map('map', {
@@ -56,38 +41,24 @@ const init = () => {
         clusterBalloonPagerSize: 100,
     });
 
-    // oldPlacemarks.forEach(item => {
-    //     const placemark = createPlacemark(item.item.coord, item.item);
-
-    //     clusterer.add(placemark);
-    //     map.geoObjects.add(clusterer);
-    // });
-
     map.events.add('click', e => {
         const currentCoords = e.get('coords');
 
-        const myGeocoder = ymaps.geocode(currentCoords, {
-            results: 1,
-            json: true
-        });
+        // const myGeocoder = ymaps.geocode(currentCoords, {
+        //     results: 1,
+        //     json: true
+        // });
 
-        // const myGeocoder = geocoder(currentCoords);
+        const myGeocoder = geocoder(currentCoords);
         
         myGeocoder.then(function (res) {
+            console.log(res);
             const obj = res.GeoObjectCollection.featureMember[0].GeoObject;
             const placemark = createPlacemark(currentCoords, obj);
 
             clusterer.add(placemark);
             map.geoObjects.add(clusterer);
             
-            // const saveObj = {
-            //     name: obj.name,
-            //     description: obj.description,
-            //     coord: currentCoords
-            // }
-
-            // placemarks.push({item: saveObj, reviews: []})
-            // localStorage.setItem('placemarks', JSON.stringify(placemarks));
         }, function (err) {
             // Обработка ошибки.
         });
