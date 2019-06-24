e96507de24f4501622b411a109dd8f3bd00d35e8
sergeybarbolin <sergeybarbolin25@gmail.com>
2019-06-24 23:27:50 +0300

    save in localStorage

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 import { resolve } from "url";
 
-const createPlacemark = (coords, geoObject) => {
+const addNewPlacemark = (coords, geoObject, map, clusterer) => {
     let placemark = new ymaps.Placemark(coords, {
         balloonContentHeader: geoObject.name,
         balloonContentBody: geoObject.description,
-        balloonContentFooter: "Подвал",
-        hintContent: "Хинт метки"
+        balloonContentFooter: 'Подвал',
+        hintContent: 'Хинт метки'
     });
 
     placemark.events.add('click', event => {
         event.preventDefault();
-        console.log('etst');
     });
     
-    // map.geoObjects.add(placemark);
+    clusterer.add(placemark);
+    map.geoObjects.add(clusterer);
 
-    return placemark;
+    return true;
 };
 
-const geocoder = coords => {
-    const coordsStr = coords.reverse().join(',');
-    const url = `https://geocode-maps.yandex.ru/1.x/?apikey=963c3670-287a-487a-8c60-9f1d43c03028&format=json&results=1&geocode=${coordsStr}`;
+// const geocoder = coords => {
+//     const coordsStr = coords.reverse().join(',');
+//     const url = `https://geocode-maps.yandex.ru/1.x/?apikey=963c3670-287a-487a-8c60-9f1d43c03028&format=json&results=1&geocode=${coordsStr}`;
     
-    return fetch(url).then(response => response.json()).then(response => response.response);
+//     return fetch(url).then(response => response.json()).then(response => response.response);
+// }
+
+const saveInLocalStorage = (key, arr) => {
+    localStorage.setItem(key, JSON.stringify(arr));
 }
 
-const init = () => {
+const init = existingPlacemarks => {
     const map = new ymaps.Map('map', {
         center: [59.938892, 30.315221],
         zoom: 15,
@@ -41,24 +45,39 @@ const init = () => {
         clusterBalloonPagerSize: 100,
     });
 
+    if (existingPlacemarks.length) {
+        
+        existingPlacemarks.forEach(element => {
+            let { coords, placemarkInfo } = element;
+
+            addNewPlacemark(coords, placemarkInfo, map, clusterer); 
+        });
+    } 
+
     map.events.add('click', e => {
         const currentCoords = e.get('coords');
 
-        // const myGeocoder = ymaps.geocode(currentCoords, {
-        //     results: 1,
-        //     json: true
-        // });
+        const myGeocoder = ymaps.geocode(currentCoords, {
+            results: 1,
+            json: true
+        });
 
-        const myGeocoder = geocoder(currentCoords);
+        // const myGeocoder = geocoder(currentCoords);
         
         myGeocoder.then(function (res) {
             console.log(res);
-            const obj = res.GeoObjectCollection.featureMember[0].GeoObject;
-            const placemark = createPlacemark(currentCoords, obj);
+            const responseData = res.GeoObjectCollection.featureMember[0].GeoObject;
 
-            clusterer.add(placemark);
-            map.geoObjects.add(clusterer);
+            const placemarkInfo = {
+                name: responseData.name,
+                description: responseData.description,
+            }
             
+            addNewPlacemark(currentCoords, placemarkInfo, map, clusterer);
+            existingPlacemarks.push({ coords: currentCoords, placemarkInfo: placemarkInfo });
+
+            saveInLocalStorage('placemarks', existingPlacemarks);
+
         }, function (err) {
             // Обработка ошибки.
         });
@@ -66,4 +85,9 @@ const init = () => {
 
 }
 
-ymaps.ready(init)
+ymaps.ready(() => {
+    const oldPlacemarks = localStorage.getItem('placemarks') ? JSON.parse(localStorage.getItem('placemarks')) : [];
+
+    init(oldPlacemarks);
+});
+
