ea8bd5a3b34f387cd86fd1edeee7bdbb062d5c5c
sergeybarbolin <sergeybarbolin25@gmail.com>
2019-07-02 00:20:57 +0300

    bug position popup

--- src/index.js
+++ src/index.js
@@ -14,10 +14,21 @@ const addNewPlacemark = (coords, address, review = {}) => {
 };
 
 const createClusterer = () => {
+    const customClusterBalloonContent = ymaps.templateLayoutFactory.createClass(
+        `
+            <div class="balloon">
+                <a href="#">{{ properties.balloonContentHeader|raw }}</a>
+                <div class="balloon__body">{{ properties.balloonContentBody|raw }}</div>
+                <div class="balloon__footer">{{ properties.balloonContentFooter|raw }}</div>
+            </div>
+        `
+    );
+
     return new ymaps.Clusterer({
         clusterDisableClickZoom: true,
         clusterOpenBalloonOnClick: true,
         clusterBalloonContentLayout: 'cluster#balloonCarousel',
+        clusterBalloonItemContentLayout: customClusterBalloonContent,
         clusterBalloonPanelMaxMapArea: 0,
         clusterBalloonContentLayoutWidth: 200,
         clusterBalloonContentLayoutHeight: 130,
@@ -43,6 +54,9 @@ const placemarksStorage = {
     },
     get getAll() {
         return this._placemarks;
+    },
+    getItem: function(key) {
+        return this._placemarks[key];
     },
     forAll: function(fn) {
         const existingPlacemarks = this.getAll;
@@ -81,6 +95,16 @@ const formHandler = formIdentifier => {
     return data;
 }
 
+const formClear = formIdentifier => {
+    const form = document.querySelector(formIdentifier);
+
+    for (const el of form.children) {
+        if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA') {
+            el.value = '';
+        } 
+    }
+}
+
 const geocoder = coords => {
     const coordsStr = coords.slice().reverse().join(',');
     const url = `https://geocode-maps.yandex.ru/1.x/?apikey=963c3670-287a-487a-8c60-9f1d43c03028&format=json&results=1&geocode=${coordsStr}`;
@@ -90,12 +114,58 @@ const geocoder = coords => {
         .then(response => response.response.GeoObjectCollection.featureMember[0].GeoObject.metaDataProperty.GeocoderMetaData.text)
 }
 
+const renderReviews = (popup, address) => {
+    const key = deleteCharacters(address);
+    const dataItem = placemarksStorage.getItem(key);
+    const reviews = dataItem ? dataItem.reviews : [];
+    const reviewWrapper = popup.querySelector('.reviews');
+
+    formClear('.form');
+    reviewWrapper.innerHTML = '';
+
+    reviews.forEach(item => {
+        let reviewItem = document.createElement('p');
+        let textReview = `Имя: ${item.review.firstName} Фамилия: ${item.review.secondName} Отзыв: ${item.review.review}`;
+        
+        reviewItem.innerText = textReview; 
+        reviewWrapper.prepend(reviewItem);
+    });
+}
+
+const displayPopup = (popup, mapWrapper, position, map) => {
+    
+    const positionCenter = map.getGlobalPixelCenter();
+
+    popup.style.left = position.x - popup.offsetWidth - 5 + 'px'
+    popup.style.top = position.y - popup.offsetHeight - 5 + 'px';
+    map.setGlobalPixelCenter([positionCenter[0], positionCenter[1] - popup.offsetHeight + 5]);
+
+    // if (position.x > mapWrapper.offsetWidth / 2) {
+    //     popup.style.left = position.x - popup.offsetWidth - 5 + 'px'
+    //     // map.setGlobalPixelCenter([positionCenter[0] + popup.offsetWidth + 5, positionCenter[1]]);
+    // } else {
+    //     popup.style.left = position.x + 5 + 'px';
+    //     // map.setGlobalPixelCenter([positionCenter[0] - popup.offsetWidth - 5, positionCenter[1]]);
+    // }
+
+    // if (position.y > mapWrapper.offsetHeight / 2) {
+    //     popup.style.top = position.y - popup.offsetHeight - 5 + 'px';
+    //     map.setGlobalPixelCenter([positionCenter[0] - popup.offsetWidth - 5, positionCenter[1] + popup.offsetHeight + 5]);
+    // } else {
+    //     popup.style.top = position.y + 5 + 'px';
+    //     map.setGlobalPixelCenter([positionCenter[0], positionCenter[1] - popup.offsetHeight - 5]);
+    // }
+
+    popup.classList.add('popup--visible');
+
+
+
+    return true;
+}
+
 ymaps.ready(() => {
     const map = init();
-    const mapWrapper = document.querySelector('#map');
-    
     const clusterer = createClusterer();
-    const popup = document.querySelector('.popup');
     let coords = null;
     let address = null;
 
@@ -109,46 +179,47 @@ ymaps.ready(() => {
 
     map.geoObjects.events.add('click', e => {
         const elementName = e.get('target').options._name;
-
-        if (elementName === 'geoObject') {
-            e.preventDefault();
-        } else if (elementName === 'cluster') {
-            console.log('sad');
-        }
-    });
-
-    map.events.add('click', e => {
-        coords = e.get('coords');
         const position = {
             x: e.get('domEvent').get('pageX'),
             y: e.get('domEvent').get('pageY')
         }
 
-        popup.style.left = (position.x > mapWrapper.offsetWidth / 2) ?
-            popup.style.left = position.x - popup.offsetWidth - 5 + 'px' :
-            popup.style.left = position.x + 5 + 'px';
+        address = e.get('target').properties.get('balloonContentHeader');
+        coords = e.get('coords');
 
-        popup.style.top = (position.y > mapWrapper.offsetHeight / 2) ?
-            popup.style.top = position.y - popup.offsetHeight - 5 + 'px' :
-            popup.style.top = position.y + 5 + 'px';
+        clusterer.balloon.close();
+
+        if (elementName === 'geoObject') {
+            e.preventDefault();
+
+            displayPopup(popup, mapWrapper, position, map);
+            renderReviews(popup, address);
+
+        } else if (elementName === 'cluster') {
+            popup.classList.remove('popup--visible');
+        }
+    });
+
+    map.events.add('click', e => {
+        const position = {
+            x: e.get('domEvent').get('pageX'),
+            y: e.get('domEvent').get('pageY')
+        }
+
+        coords = e.get('coords');
+
+        clusterer.balloon.close();
 
         geocoder(coords).then(response => {
             address = response;
-
-            const specificReviews = placemarksStorage.getAll[deleteCharacters(address)].reviews;
-
-            // console.log(specificReviews[0].review.firstName);
-            specificReviews.forEach(item => {
-                let reviewWrapper = document.createElement('p');
-                let textReview = `Имя: ${item.review.firstName} Фамилия: ${item.review.secondName} Отзыв: ${item.review.review}`
-            
-                reviewWrapper.innerText = textReview; 
-                popup.prepend(reviewWrapper);
-            })
-            popup.style.display = 'block';
+            displayPopup(popup, mapWrapper, position, map);
+            renderReviews(popup, address);
         });
         
     });
+
+    const popup = document.querySelector('.popup');
+    const mapWrapper = document.querySelector('#map');
 
     popup.addEventListener('click', e => {
         const formData = formHandler('.form');
@@ -158,9 +229,27 @@ ymaps.ready(() => {
 
             clusterer.add(placemark);
             placemarksStorage.add(coords, address, formData);
+            renderReviews(popup, address);
         }
         
-    })
+    });
+
+    mapWrapper.addEventListener('click', e => {
+        e.preventDefault();
+
+        if (e.target.nodeName === 'A') {
+            const position = {
+                x: e.clientX,
+                y: e.clientY
+            }
+
+            // console.log(position);
+            clusterer.balloon.close();
+            displayPopup(popup, mapWrapper, position, map);
+            renderReviews(popup, e.target.innerText);
+        }
+        
+    });
 
 });
 
